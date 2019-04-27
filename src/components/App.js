a801ead3a4600c7833824f763312e716504c27ad
iuliancarnaru <iulian.carnaru@yahoo.co.uk>
2019-04-27 19:14:19 +0100

    sorting API requests

--- src/components/App.js
+++ src/components/App.js
@@ -4,22 +4,35 @@ import axios from "axios";
 const App = () => {
   const [apiData, setApiData] = useState([]);
   const apiKey = process.env.REACT_APP_API_KEY;
+  const searchWord = "";
+
+  const endpoints = {
+    content: "search",
+    tags: "tags",
+    sections: "sections",
+    editions: "editions", // At current we have editions for the United Kingdom, the United States and Australia.
+    singleItem: "technology" // The single item endpoint returns all the data we have for a given single item id. Here the term 'item' refers to either a piece of content, a tag, or a section.
+  };
+
+  // The q parameter supports AND (,), OR (|) and NOT (-) operators
 
   useEffect(() => {
     axios
-      .get(`https://content.guardianapis.com/search?api-key=${apiKey}`)
+      .get(
+        `https://content.guardianapis.com/${
+          endpoints.content
+        }?page=1&q=${searchWord}&api-key=${apiKey}`
+      )
       .then(result => setApiData(result.data.response.results));
-  }, [apiKey]);
-
-  console.log(apiData);
+  }, [apiKey, endpoints.content]);
 
   return (
     <div>
       <h1>The Guardian App</h1>
       <ul>
         {apiData.map(item => (
-          <a href={`${item.webUrl}`}>
-            <li key={item.id}>{item.webTitle}</li>
+          <a key={item.id} href={`${item.webUrl}`}>
+            <li>{item.webTitle}</li>
           </a>
         ))}
       </ul>
