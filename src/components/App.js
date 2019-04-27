2827bad8b44dfd2b8de3cd2fd41d3748f3d04cab
iuliancarnaru <iulian.carnaru@yahoo.co.uk>
2019-04-27 15:29:48 +0100

    adding data to dispaly

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,13 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const App = () => {
-  const [apiData, setApiData] = useState({});
+  const [apiData, setApiData] = useState([]);
   const apiKey = process.env.REACT_APP_API_KEY;
 
   useEffect(() => {
     axios
       .get(`https://content.guardianapis.com/search?api-key=${apiKey}`)
-      .then(result => setApiData(result.data));
+      .then(result => setApiData(result.data.response.results));
   }, [apiKey]);
 
   console.log(apiData);
@@ -16,6 +16,13 @@ const App = () => {
   return (
     <div>
       <h1>The Guardian App</h1>
+      <ul>
+        {apiData.map(item => (
+          <a href={`${item.webUrl}`}>
+            <li key={item.id}>{item.webTitle}</li>
+          </a>
+        ))}
+      </ul>
     </div>
   );
 };
